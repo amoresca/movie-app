46a6a696e549c2e6668db77662ff190fcba971f8
amoresca <casee.amores@gmail.com>
2021-01-14 18:37:42 -0500

    shows all movies in alphabetical order rather than ordered by id

--- app/controllers/api/movies_controller.rb
+++ app/controllers/api/movies_controller.rb
@@ -1,11 +1,11 @@
 class Api::MoviesController < ApplicationController
   def view_all
-    @movies = Movie.all
+    @movies = Movie.order(title: :asc)
     render "view_all.json.jb"
   end
 
   def view_single
-    @movie = Movie.first
+    @movie = Movie.find(7)
     render "view_single.json.jb"
   end
 end
