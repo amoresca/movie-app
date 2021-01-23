9b66680411c7b1d238d3f000eee45a8ef29fc02c
amoresca <casee.amores@gmail.com>
2021-01-23 10:17:13 -0500

    modifies movie and actor index actions

--- app/controllers/api/movies_controller.rb
+++ app/controllers/api/movies_controller.rb
@@ -1,6 +1,6 @@
 class Api::MoviesController < ApplicationController
   def index
-    @movies = Movie.order(title: :asc)
+    @movies = Movie.where("english = ?", true).order(title: :asc)
     render "index.json.jb"
   end
 
