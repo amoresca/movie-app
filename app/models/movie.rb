1c05b1a7e876bcd5895bfa0e50bda281e7002b21
amoresca <casee.amores@gmail.com>
2021-01-30 10:49:07 -0500

    adds genres to movies

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,15 @@
 class Movie < ApplicationRecord
 
   has_many :actors
+  has_many :movie_genres
+  has_many :genres, through: :movie_genres
   
   validates :title, presence: true
   validates :year, length: { is: 4, wrong_length: "must have %{count} digits" }
+
+  def genre_names
+    genres.map do |genre|
+      genre.name
+    end
+  end
 end
