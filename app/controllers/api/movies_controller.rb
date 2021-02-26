6cb234c07cb8aca147744baa64febb247188a642
amoresca <casee.amores@gmail.com>
2021-02-25 19:49:30 -0500

    adds image url to movies

--- app/controllers/api/movies_controller.rb
+++ app/controllers/api/movies_controller.rb
@@ -14,7 +14,8 @@ class Api::MoviesController < ApplicationController
       year: params[:year],
       plot: params[:plot],
       director: params[:director],
-      english: params[:english]
+      english: params[:english],
+      image_url: params[:image_url]
     )
 
     if @movie.save
@@ -44,6 +45,7 @@ class Api::MoviesController < ApplicationController
     @movie.plot = params[:plot] || @movie.plot
     @movie.director = params[:director] || @movie.director
     @movie.english = params[:english] || @movie.english
+    @movie.image_url = params[:image_url] || @movie.image_url
     
     if @movie.save
       render "show.json.jb"
